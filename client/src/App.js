8b056c4bb83f49e3b458dac8a464461c355ea830
franciscoe <franslopezcortes@gmail.com>
2021-09-08 19:49:12 -0300

    auth0

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import LoginButton from './login';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Henry Countries</h1>
+      <LoginButton/>
     </div>
   );
 }
