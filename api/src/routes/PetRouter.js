0a89fcf5e9c35e50e4f3db8801e8d5549d48370d
GabrielPeiteado <gabyypeiteado@gmail.com>
2021-09-08 19:46:34 -0300

    finished routes & controllers

--- api/src/routes/PetRouter.js
+++ api/src/routes/PetRouter.js
@@ -4,10 +4,16 @@ const {
     createPet,
     getAllPets,
     getPetById,
+    getPetByUserId,
+    deletePetById,
+    editPet
 } = require('../controllers/PetController');
 
 server.post('/createPet', createPet);
 server.get('/getAllPets', getAllPets);
 server.get('/getPetById/:id', getPetById);
+server.get('/getPetByUserId/:id', getPetByUserId);
+server.delete('/deletePetById/:id', deletePetById);
+server.put('/editPet/:id', editPet);
 
 module.exports = server;
