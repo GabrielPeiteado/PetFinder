99788ffb6f99cea3a520617d39a555e71d69157f
franciscoe <franslopezcortes@gmail.com>
2021-09-08 19:47:52 -0300

    auth0

---
+++ client/src/login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import "./login.css"
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+  return (
+    <button 
+      className="landP" 
+      onClick={() => loginWithRedirect()}>
+      <h3>Log In</h3>
+    </button>
+  );
+};
+
+export default LoginButton;
