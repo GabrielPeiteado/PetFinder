862595b387e4c99b684d1ccc4018dfaf82cc360d
franciscoe <franslopezcortes@gmail.com>
2021-09-09 18:28:17 -0300

    auth0

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
+import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from './Components/Login/login';
 import LogoutButton from './Components/Logout/Logout';
-
+import { Profile } from './Components/Porfile/porfile';
 
 function App() {
+  const { isAuthenticated } = useAuth0();
   return (
     <div className="App">
-      <h1>Henry Countries</h1>
-      <LoginButton/>
-      <LogoutButton/>
-    </div>
+    <header className="App">
+      {isAuthenticated ? (
+        <>
+          <Profile />
+          <LogoutButton />
+        </>
+      ) : (
+        <LoginButton />
+      )}
+    </header>
+  </div>
   );
 }
 
